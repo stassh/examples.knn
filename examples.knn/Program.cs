b7f4403d5f75befaa39c6e8730ee1d3523329223
Stanislav Shabalin <Stanislav.Shabalin@exigenservices.com>
2018-03-28 12:54:13 +0300

    Initial commit

---
+++ examples.knn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examples.knn
+{
+  /// <summary>
+  /// KNN algorithm
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
